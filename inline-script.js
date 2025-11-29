1fc0f0aea1c95135b4f022fdca0cb421ad88128a
satriabackupzxdeployz <satriabackupz@gmail.com>
2025-11-29 20:49:53 +0700

    Add inline-script.js

---
+++ inline-script.js
@@ -0,0 +1,30 @@
+{
+        "@context": "https://schema.org/",
+        "@type": "BreadcrumbList",
+        "itemListElement": [
+          {
+            "@type": "ListItem",
+            "position": 1,
+            "name": "Dark jokes & dark humor",
+            "item": "https://lahelu.com/tag/dark"
+          },
+          {
+            "@type": "ListItem",
+            "position": 2,
+            "name": "Trending",
+            "item": "https://lahelu.com/trending"
+          },
+          {
+            "@type": "ListItem",
+            "position": 3,
+            "name": "Fresh",
+            "item": "https://lahelu.com/fresh"
+          },
+          {
+            "@type": "ListItem",
+            "position": 4,
+            "name": "Leaderboard",
+            "item": "https://lahelu.com/leaderboard"
+          }
+        ]
+      }
